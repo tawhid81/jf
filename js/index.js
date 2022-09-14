9515efc3abcef2bd5c2c0866698b6b77a68e1c3c
tawhid81 <111502716+tawhid81@users.noreply.github.com>
2022-09-14 12:07:35 +0100

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,8 @@
+var price = 33; 
+var fname ='shabana';
+var boxname ='cocola';
+var price = 88_34;
+var enmu = 1;
+var $box_monika = 78;
+var home_address = "kochu khete";
+
---
+++ js/practice1.js
@@ -0,0 +1,22 @@
+
+if (score >= 90) {
+grade = 'A+';
+}
+else if (score >= 80) {
+    geade = 'A';
+}
+else if (score >= 70) {
+    grade = 'B';
+}
+
+else if (score >= 60) {
+    grade = 'C';
+}
+
+else if (score >= 50) {
+    grade = 'D';
+}
+else {
+    grade = 'F';
+}
+console.log (+ grade);
